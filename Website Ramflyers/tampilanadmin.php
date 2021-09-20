e1ce051628a9c73a035744215c521ff5cbd3fbd1
Janssen125 <janssenaddisonchen@gmail.com>
2021-09-20 11:45:06 +0700

    Create tampilanadmin.php
    COMMIT

---
+++ Website Ramflyers/tampilanadmin.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <style>
+            .header{
+                float:left;
+                text-decoration:none;
+                list-style:none;
+                padding:25px;
+                margin-top:0px;
+                background-color:grey;
+                font-family:"Nunito Sans";
+                height:40px;
+            }
+            .top{
+                margin-left:-50px;
+                margin-top:-10px;
+            }
+            </style>
+</head>
+<body>
+    <div class=top>
+        <ul>
+            <li class=header>
+                <h1> ADMINISTRATOR </h1>
+        </li>
+<li class=header>
+    A
+</li>
+<li class=header>
+    B
+</li>
+</div>
+</body>
+</html>
