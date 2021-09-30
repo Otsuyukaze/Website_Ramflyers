120900a072a91c1dd090e2ee1a1ee25ff5d37948
fxdeking <hendytandika25@gmail.com>
2021-09-30 22:45:02 +0700

    gg

--- Website Ramflyers/simpan1.php
+++ Website Ramflyers/simpan1.php
@@ -9,5 +9,5 @@ $message = $_POST['message'];
 $sql="insert into hubungi(nama,email,subject,message) values ('$nama','$email','$subject','$message')";
 
 mysqli_query($db,$sql) or die (mysqli_error());
-header('location:ty.html');
+header('location:ty.php');
 ?>
